9260d76f6fdb3f16e08ec38546432267be22705e
Hans Hoffman <hoffman.hans.j@gmail.com>
2023-06-11 20:41:51 -0600

    start to break execution up into separate paths

--- src/Run.hs
+++ src/Run.hs
@@ -7,10 +7,20 @@ import Types
 -- import System.Directory (getHomeDirectory) -- https://hackage.haskell.org/package/directory-1.3.8.1/docs/System-Directory.html
 -- import System.FilePath ((</>)) -- https://hackage.haskell.org/package/filepath-1.4.100.3/docs/System-FilePath.html
 
-run :: RIO App ()
-run = do
+init :: Template -> RIO App ()
+init = logInfo . (<>) "init " . displayShow
+
+publish :: FilePath -> RIO App ()
+publish targetFile = do
   env <- ask
-  logInfo $ displayShow (view optsL env)
+  logInfo $ "publish " <> displayShow targetFile
   res <- Api.Environment.get (view flatfileEnvIdL env)
   logInfo $ displayShow res
   logInfo "Done!! 🎉"
+
+run :: RIO App ()
+run = do
+  env <- ask
+  case (aoCommand $ view optsL env) of
+    Init template -> init template
+    Publish targetFile -> publish targetFile
